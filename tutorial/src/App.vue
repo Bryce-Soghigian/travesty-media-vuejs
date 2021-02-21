4adc1259909ffe8ff3e5b394e2cd0c1ab58b9354
Bryce-Soghigian <brybrycodingguy@gmail.com>
2021-02-20 19:12:31 -0600

    basic data rendering

---
+++ tutorial/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <todos v-bind:todos="todos"/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue';
+export default {
+  name: "App",
+  components: {Todos},
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "FIRST TODO",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Second TODO",
+          completed: false,
+        },
+        {
+          id: 3,
+          title: "Third TODO",
+          completed: true,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
